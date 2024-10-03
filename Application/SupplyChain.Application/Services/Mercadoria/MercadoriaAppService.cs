8fbf15c0a54779f99dd98a1e115c88b4f533ce7b
joseNote <jose.csousa22@gmail.com>
2024-10-03 20:21:34 -0300

    feat(Mercadoria): Entrada e atualização de estoque

--- Application/SupplyChain.Application/Services/Mercadoria/MercadoriaAppService.cs
+++ Application/SupplyChain.Application/Services/Mercadoria/MercadoriaAppService.cs
@@ -3,6 +3,7 @@ using SupplyChain.Application.Interfaces;
 using SupplyChain.Application.ValueObjects.Dto.Mercadoria;
 using SupplyChain.Application.ValueObjects.ViewModels.Mercadoria;
 using SupplyChain.Domain.Bus;
+using SupplyChain.Domain.Entities;
 using SupplyChain.Domain.Interface.Repository;
 using MercadoriaDomain = SupplyChain.Domain.Entities.Mercadoria;
 
@@ -42,7 +43,7 @@ public partial class MercadoriaAppService : IMercadoriaAppService
         {
             return null;
         }
-
+        
         return _mapper.Map<CriarMercadoriaViewModel>(mercadoria, options =>
         {
             options.Items["NomeDoTipoMercadoria"] = tipoDeMercadoria?.Nome;
@@ -73,10 +74,50 @@ public partial class MercadoriaAppService : IMercadoriaAppService
         {
             return null;
         }
-        
+
+        EstoqueCadastroATTRabbitmq(entrada);
+
         return _mapper.Map<CadastrarEntradaViewModel>(entrada, options =>
         {
             options.Items["NomeDaMercadoria"] = mercadoria.Nome;
         });
+    }
+
+    private void EstoqueCadastroATTRabbitmq(Entrada entrada)
+    {
+        //Logica do estoque mandar para fila depois
+        var estoque = _repository.Query<Estoque>(x => x.Local.ToLower().Equals(entrada.Local.ToLower()))
+                                 .FirstOrDefault();
+                 
+        if (estoque == null)
+        {
+            CriarEstoque(entrada);
+        }
+        
+        else
+        {
+            AtualizarEstoque(estoque);
+        }
+
+        SaveChanges();
+    }
+
+    private void AtualizarEstoque(Estoque estoque)
+    {
+        estoque.AdicionarEstoque(estoque.Quantidade);
+        _repository.Update(estoque);
+    }
+
+    private bool CriarEstoque(Entrada entrada)
+    {
+        var novoEstoque = new Estoque(entrada.Local, entrada.Quantidade, entrada.MercadoriaId);
+            
+        if (!Validar(entrada))
+        {
+            return true;
+        } 
+            
+        _repository.Add(novoEstoque);
+        return false;
     }
 }
--- Domain/SupplyChain.Domain/Entities/Estoque.cs
+++ Domain/SupplyChain.Domain/Entities/Estoque.cs
@@ -4,12 +4,24 @@ namespace SupplyChain.Domain.Entities;
 
 public class Estoque : BaseEntity
 {
-    public string Local { get; set; }
-    public int Quantidade { get; set; }
+    public string Local { get; private set; }
+    public int Quantidade { get; private set; }
     
-    public Guid MercadoriaId { get; set; }
-    public Mercadoria Mercadoria { get; set; }
+    public Guid MercadoriaId { get; private set; }
+    public Mercadoria Mercadoria { get; private set; }
 
+    public Estoque(string local, int quantidade, Guid mercadoriaId)
+    {
+        Local = local;
+        Quantidade = quantidade;
+        MercadoriaId = mercadoriaId;
+    }
+
+    public void AdicionarEstoque(int quantidade)
+    {
+        Quantidade += quantidade;
+    }
+    
     public override (bool IsValid, IDictionary<string, string> Erros) Validate()
     {
         var erros = new Dictionary<string, string>();
